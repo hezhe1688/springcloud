64a94dcbca93aa097340ba87c8883dee865b252c
贺哲 <hezhe1688@163.com>
2020-02-10 21:32:22 +0800

    实现服务发现

--- microservicecloud-provider-dept-8001/src/main/java/com/hezhe/microservicecloud/DeptProvider8001_Config_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/hezhe/microservicecloud/DeptProvider8001_Config_App.java
@@ -3,13 +3,13 @@ package com.hezhe.microservicecloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient //开启eureka客户端
+@EnableEurekaClient //开启eureka客户端,本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient //服务发现
 @SpringBootApplication
 @MapperScan("com.hezhe.microservicecloud.mapper")
-/*@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
-@EnableDiscoveryClient //服务发现*/
 public class DeptProvider8001_Config_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_Config_App.class, args);
--- microservicecloud-provider-dept-8001/src/main/java/com/hezhe/microservicecloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/hezhe/microservicecloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.hezhe.microservicecloud.controller;
 import com.hezhe.microservicecloud.model.Dept;
 import com.hezhe.microservicecloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,8 +15,8 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
-    /*@Autowired
-    private DiscoveryClient client;*/
+    @Autowired
+    private DiscoveryClient client;
 
     @PostMapping("/dept/add")
     public boolean add(@RequestBody Dept dept) {
@@ -31,17 +33,18 @@ public class DeptController {
         return deptService.list();
     }
 
-//    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
-//    public Object discovery() {
-//        List<String> list = client.getServices();
-//        System.out.println("**********" + list);
-//
-//        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
-//        for (ServiceInstance element : srvList) {
-//            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
-//                    + element.getUri());
-//        }
-//        return this.client;
-//    }
+    //服务发现
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
 
 }
