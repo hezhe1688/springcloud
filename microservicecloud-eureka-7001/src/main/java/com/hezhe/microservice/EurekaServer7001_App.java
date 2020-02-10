828b21252b85f483ace94cff603d36ad07a44558
贺哲 <hezhe1688@163.com>
2020-02-10 20:10:42 +0800

    eureka服务注册与实现

---
+++ microservicecloud-eureka-7001/src/main/java/com/hezhe/microservice/EurekaServer7001_App.java
@@ -0,0 +1,16 @@
+package com.hezhe.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 提供服务注册和发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7001_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001_App.class,args);
+    }
+}
